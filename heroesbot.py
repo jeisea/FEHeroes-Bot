6beb103346937426b33210c0f0414102b1f23107
Jacky Tse <jackytse@Jackys-MacBook-Pro.local>
2017-06-19 01:39:02 -0400

    Finish most of wiki response parsing

--- heroesbot.py
+++ heroesbot.py
@@ -6,27 +6,23 @@ import pdb
 import re
 import praw
 import GamePedia
-import GamePress
-import data
+import Reply
+
 REDDIT = praw.Reddit("bot")
 SUBREDDIT = REDDIT.subreddit("FireEmblemHeroes")
 
 def start_bot():
     """Does stuff"""
+    all_data = []
     for comment in SUBREDDIT.stream.comments():
         for match in re.finditer("<<([^>]*)>>", comment.body):
             query = match.group(1)
             gamepedia_data = GamePedia.search_gamepedia(query)
-            gamepress_data = GamePress.search_gamepress(query)
-            comment.reply(build_reply(query))
- 
-    if not os.path.isfile("posts_replied_to.txt"):
-        posts_replied_to = []
-    else:
-        with open("posts_replied_to.txt", "r") as replied_posts:
-            posts_replied_to = replied_posts.read()
-            posts_replied_to = posts_replied_to.split("\n")
-            posts_replied_to = list(filter(None, posts_replied_to))
+            if gamepedia_data:
+                all_data.append(gamepedia_data)
+        if (len(all_data) > 0):
+            comment.reply(Reply.build_reply(all_data))
+            del all_data[:]
 
 def build_reply(query):
     reply = "Here is more information about " + query
